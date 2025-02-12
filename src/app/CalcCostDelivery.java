e7bfeb363c18159ecf842dec66601cac65ecf4f4
Ed Gluschenko <edgluschenko3@gmail.com>
2025-02-12 21:28:24 +0200

    change name variables

--- src/app/CalcCostDelivery.java
+++ src/app/CalcCostDelivery.java
@@ -2,7 +2,7 @@ package app;
 
 public class CalcCostDelivery extends CalcCostBase{
 
-    private final static double deliveryPrice=7;
+    private final static double DELIVERYPRICE=7;
 
     public double calcCost(Product product){
         return product.getQuota() * product.getPrice() + deliveryPrice;
